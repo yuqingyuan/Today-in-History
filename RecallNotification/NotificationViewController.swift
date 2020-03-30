dae07ad307d095aeae1648311ca344740a3b71f0
yuqingyuan <2514171352@qq.com>
2020-03-30 17:43:31 +0800

    初始化通知模块

---
+++ RecallNotification/NotificationViewController.swift
@@ -0,0 +1,26 @@
+//
+//  NotificationViewController.swift
+//  RecallNotification
+//
+//  Created by yuqingyuan on 2020/3/30.
+//  Copyright © 2020 俞清源. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+import UserNotificationsUI
+
+class NotificationViewController: UIViewController, UNNotificationContentExtension {
+
+    @IBOutlet var label: UILabel?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any required interface initialization here.
+    }
+    
+    func didReceive(_ notification: UNNotification) {
+        self.label?.text = notification.request.content.body
+    }
+
+}
